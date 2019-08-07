33d5302c9bf7d198bd112111ba9cffaaa1b111d5
Laurence Lin <laurence.lin@icloud.com>
2019-08-07 11:28:12 -0400

    minor correction

--- ssurgo_soiltexture2gis.R
+++ ssurgo_soiltexture2gis.R
@@ -4,8 +4,8 @@ arg=commandArgs(T)
 library(rgrass7)
 
 rast = readRAST('soil_ssurgo')
-soil_cat_mukey = arg[1] # product from "v.db.select map=ssurgo separator=comma file=$PROJDIR/rhessys/ssurgo_cat_mukey.csv"
-soil_mukey_texture = arg[2] # product from "Rscript ssurgo_extraction.R $downloadedSSURGO_directory"
+soil_cat_mukey = arg[1] 
+soil_mukey_texture = arg[2]
 
 cat_mukey = read.csv(soil_cat_mukey, stringsAsFactors=F)
 mukey_texture = read.csv(soil_mukey_texture, stringsAsFactors=F)
