88e740542f6618e25c43d273f5b3bac2cc3f3de1
Laurence Lin <laurence.lin@icloud.com>
2019-09-04 16:01:28 -0400

    update soil2grass

--- ssurgo_soiltexture2gis.R
+++ ssurgo_soiltexture2gis.R
@@ -19,16 +19,16 @@ mukey_texture = read.csv(soil_mukey_texture, stringsAsFactors=F)
 	rast$soil_texture = as.integer(cat_texture[cond2]); writeRAST(rast,'soil_texture',zcol='soil_texture',overwrite=T)
 	
 	## more variable
-	cat_texture = mukey_texture[cond,'ksat0']
+	cat_texture = mukey_texture[cond,'ksat_0']
 	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_ksat_0',zcol='soil_texture',overwrite=T)
 	
-	cat_texture = mukey_texture[cond,'ksat0_decay']
+	cat_texture = mukey_texture[cond,'ksat_decay']
 	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_ksat_decay',zcol='soil_texture',overwrite=T)
 	
-	cat_texture = mukey_texture[cond,'por0']
+	cat_texture = mukey_texture[cond,'por_0']
 	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_por_0',zcol='soil_texture',overwrite=T)
 	
-	cat_texture = mukey_texture[cond,'por0_decay']
+	cat_texture = mukey_texture[cond,'por_decay']
 	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_por_decay',zcol='soil_texture',overwrite=T)
 	
 	cat_texture = mukey_texture[cond,'sand']
@@ -42,7 +42,10 @@ mukey_texture = read.csv(soil_mukey_texture, stringsAsFactors=F)
 	
 	cat_texture = mukey_texture[cond,'BD']
 	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_BD',zcol='soil_texture',overwrite=T)
-	
+
+    cat_texture = mukey_texture[cond,'partdensity']
+    rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_horizonDepth',zcol='soil_texture',overwrite=T)
+
 	cat_texture = mukey_texture[cond,'soilDepth']
 	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_horizonDepth',zcol='soil_texture',overwrite=T)
 
