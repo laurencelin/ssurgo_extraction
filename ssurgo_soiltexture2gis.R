d30836cdb49a538fc7bf301ce38ae493fd45ecce
Laurence Lin <laurence.lin@icloud.com>
2019-10-10 15:59:02 -0400

    update to count for new extraction command from grass

--- ssurgo_soiltexture2gis.R
+++ ssurgo_soiltexture2gis.R
@@ -7,7 +7,13 @@ rast = readRAST('soil_ssurgo')
 soil_cat_mukey = arg[1] 
 soil_mukey_texture = arg[2]
 
-cat_mukey = read.csv(soil_cat_mukey, stringsAsFactors=F)
+cat_mukey = read.csv(soil_cat_mukey, stringsAsFactors=F,header=F)
+if( is.numeric(cat_mukey[1,1]) ){
+    colnames(cat_mukey) = c('x','y','cat','mukey')
+}else{
+    cat_mukey = read.csv(soil_cat_mukey, stringsAsFactors=F)
+}
+
 mukey_texture = read.csv(soil_mukey_texture, stringsAsFactors=F)
 	
 	# matching SSURGO dataset product to the mapping mukey
