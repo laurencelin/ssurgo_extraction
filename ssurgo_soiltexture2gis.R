65547a17791f0d7a26fbd6be2102e16dbf6fb8ad
Laurence Lin <laurence.lin@icloud.com>
2020-03-16 16:09:34 -0400

    update ssurgo_soiltexture2gis.R: remove incorrect soil info that write back to gis; add suffix feature to name the output map

--- ssurgo_soiltexture2gis.R
+++ ssurgo_soiltexture2gis.R
@@ -6,6 +6,7 @@ library(rgrass7)
 rast = readRAST('soil_ssurgo')
 soil_cat_mukey = arg[1] 
 soil_mukey_texture = arg[2]
+if(length(arg)>2){ suffix = arg[3]; } else { suffix=''; }
 
 cat_mukey = read.csv(soil_cat_mukey, stringsAsFactors=F,header=F)
 if( is.numeric(cat_mukey[1,1]) ){
@@ -22,38 +23,7 @@ mukey_texture = read.csv(soil_mukey_texture, stringsAsFactors=F)
 	
 	# sorting matched mukey to the map organization
 	cond2 = match(rast@data[[1]], cat_mukey[,'cat'])
-	rast$soil_texture = as.integer(cat_texture[cond2]); writeRAST(rast,'soil_texture',zcol='soil_texture',overwrite=T)
+	rast$soil_texture = as.integer(cat_texture[cond2]); writeRAST(rast,paste('soil_texture',suffix,sep=''),zcol='soil_texture',overwrite=T)
 	
-	## more variable
-	cat_texture = mukey_texture[cond,'ksat_0']
-	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_ksat_0',zcol='soil_texture',overwrite=T)
 	
-	cat_texture = mukey_texture[cond,'ksat_decay']
-	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_ksat_decay',zcol='soil_texture',overwrite=T)
-	
-	cat_texture = mukey_texture[cond,'por_0']
-	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_por_0',zcol='soil_texture',overwrite=T)
-	
-	cat_texture = mukey_texture[cond,'por_decay']
-	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_por_decay',zcol='soil_texture',overwrite=T)
-	
-	cat_texture = mukey_texture[cond,'sand']
-	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_sand',zcol='soil_texture',overwrite=T)
-
-	cat_texture = mukey_texture[cond,'silt']
-	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_silt',zcol='soil_texture',overwrite=T)
-	
-	cat_texture = mukey_texture[cond,'clay']
-	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_clay',zcol='soil_texture',overwrite=T)
-	
-	cat_texture = mukey_texture[cond,'BD']
-	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_BD',zcol='soil_texture',overwrite=T)
-
-    cat_texture = mukey_texture[cond,'partdensity']
-    rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_PD',zcol='soil_texture',overwrite=T)
-
-	cat_texture = mukey_texture[cond,'soilDepth']
-	rast$soil_texture = cat_texture[cond2]; writeRAST(rast,'soil_horizonDepth',zcol='soil_texture',overwrite=T)
-
-
 	
